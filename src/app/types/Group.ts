81af56a5883e59db8bd42e9424deef1075ebf4c5
AntoineCheron <cheron.antoine@gmail.com>
2017-06-15 16:53:17 +0200

    Created Group type

---
+++ src/app/types/Group.ts
@@ -0,0 +1,37 @@
+import {Node} from './node';
+
+/**
+ * Created by antoine on 15/06/2017.
+ */
+
+export class Group {
+    public group: Object;
+    public id: string;
+    public name: string;
+    public nodes: Array<Node>;
+    public metadata: Object;
+    public graph: string;
+
+    constructor (group: Object) {
+        this.id = group['id'];
+        this.name = group['name'];
+        this.metadata = group['metadata'];
+        this.graph = group['graph'];
+
+        const n: Array<Object> = group['nodes'];
+        n.forEach((node: Object) => {
+            const newNode = new Node(node);
+            this.nodes.push(newNode);
+        });
+    }
+
+    getGroup (): Object {
+        this.group['id'] = this.id;
+        this.group['name'] = this.name;
+        this.group['nodes'] = this.nodes;
+        this.group['metadata'] = this.metadata;
+        this.group['graph'] = this.graph;
+
+        return this.group;
+    }
+}
